79170abef122cab42319371795f53866d3c19695
samyaza5 <samyaza5@naver.com>
2025-05-01 09:16:37 +0900

    Update TheStack.cs

--- TheStack.cs
+++ TheStack.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class TheStack : MonoBehaviour
 {
     // Const Value
-    private const float BoundSize = 3.5f;
-    private const float MovingBoundsSize = 3f;
-    private const float StackMovingSpeed = 5.0f;
-    private const float BlockMovingSpeed = 3.5f;
-    private const float ErrorMargin = 0.1f;
-
+    private const float BoundSize = 3.5f;     
+    private const float MovingBoundsSize = 3f;   
+    private const float StackMovingSpeed = 5.0f; 
+    private const float BlockMovingSpeed = 3.5f;  
+    private const float ErrorMargin = 0.1f;        
+        
     public GameObject originBlock = null;
-
+    
     private Vector3 prevBlockPosition;
     private Vector3 desiredPosition;
     private Vector3 stackBounds = new Vector2(BoundSize, BoundSize);
@@ -21,15 +21,21 @@ public class TheStack : MonoBehaviour
 
     int stackCount = -1;
     int comboCount = 0;
-
+    
+    public Color prevColor;
+    public Color nextColor;
+    
     void Start()
     {
-        if (originBlock == null)
+        if(originBlock == null)
         {
             Debug.Log("OriginBlock is NULL");
             return;
         }
-
+        
+        prevColor = GetRandomColor();
+        nextColor = GetRandomColor();
+        
         prevBlockPosition = Vector3.down;
         Spawn_Block();
     }
@@ -40,26 +46,28 @@ public class TheStack : MonoBehaviour
         {
             Spawn_Block();
         }
-
+        
         transform.position = Vector3.Lerp(transform.position, desiredPosition, StackMovingSpeed * Time.deltaTime);
     }
-
+    
     bool Spawn_Block()
     {
         // 이전블럭 저장
-        if (lastBlock != null)
+        if(lastBlock != null)
             prevBlockPosition = lastBlock.localPosition;
-
+        
         GameObject newBlock = null;
         Transform newTrans = null;
 
         newBlock = Instantiate(originBlock);
 
-        if (newBlock == null)
+        if(newBlock == null)
         {
             Debug.Log("NewBlock Instantiate Failed!");
             return false;
         }
+
+        ColorChange(newBlock);
 
         newTrans = newBlock.transform;
         newTrans.parent = this.transform;
@@ -75,5 +83,36 @@ public class TheStack : MonoBehaviour
         lastBlock = newTrans;
 
         return true;
+    }
+    
+    Color GetRandomColor()
+    {
+        float r = Random.Range(100f, 250f) / 255f;
+        float g = Random.Range(100f, 250f) / 255f;
+        float b = Random.Range(100f, 250f) / 255f;
+
+        return new Color(r, g, b);
+    }
+    
+    void ColorChange(GameObject go)
+    {
+        Color applyColor = Color.Lerp(prevColor, nextColor, (stackCount % 11) / 10f);
+
+        Renderer rn = go.GetComponent<Renderer>();
+
+        if(rn == null)
+        {
+            Debug.Log("Renderer is NULL!");
+            return;
+        }
+
+        rn.material.color = applyColor;
+        Camera.main.backgroundColor = applyColor - new Color(0.1f, 0.1f, 0.1f);
+
+        if(applyColor.Equals(nextColor) == true)
+        {
+            prevColor = nextColor;
+            nextColor = GetRandomColor();
+        }
     }
 }
