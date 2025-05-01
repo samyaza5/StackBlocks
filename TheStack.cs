ac018d635a519cd838062ddd067e09386cedcdc7
samyaza5 <samyaza5@naver.com>
2025-05-01 09:57:58 +0900

    Update TheStack.cs

--- TheStack.cs
+++ TheStack.cs
@@ -46,7 +46,15 @@ public class TheStack : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Spawn_Block();
+            if(PlaceBlock())
+            {
+                Spawn_Block();
+            }
+            else
+            {
+                // 게임 오버
+                Debug.Log("GameOver");
+            }
         }
         
         MoveBlock();
@@ -135,4 +143,101 @@ public class TheStack : MonoBehaviour
             lastBlock.localPosition = new Vector3(secondaryPosition, stackCount, -movePosition * MovingBoundsSize);
         }
     }
+    
+    bool PlaceBlock()
+    {
+        Vector3 lastPosition = lastBlock.transform.localPosition;
+
+        if (isMovingX)
+        {
+            float deltaX = prevBlockPosition.x - lastPosition.x;
+            bool isNegativeNum = (deltaX < 0) ? true : false;
+            
+            deltaX = Mathf.Abs(deltaX);
+            if (deltaX > ErrorMargin)
+            {
+                stackBounds.x -= deltaX;
+                if (stackBounds.x <= 0)
+                {
+                    return false;
+                }
+
+                float middle = (prevBlockPosition.x + lastPosition.x) / 2;
+                lastBlock.localScale = new Vector3(stackBounds.x, 1, stackBounds.y);
+
+                Vector3 tempPosition = lastBlock.localPosition;
+                tempPosition.x = middle;
+                lastBlock.localPosition = lastPosition = tempPosition;
+                
+                float rubbleHalfScale = deltaX / 2;
+                CreateRubble(
+                    new Vector3(isNegativeNum
+                            ? lastPosition.x + stackBounds.x / 2 + rubbleHalfScale
+                            : lastPosition.x - stackBounds.x / 2 - rubbleHalfScale
+                        , lastPosition.y
+                        , lastPosition.z),
+                    new Vector3(deltaX, 1, stackBounds.y)
+                );
+            }
+            else
+            {
+                lastBlock.localPosition = prevBlockPosition + Vector3.up;
+            }
+        }
+        else
+        {
+            float deltaZ = prevBlockPosition.z - lastPosition.z;
+            bool isNegativeNum = (deltaZ < 0) ? true : false;
+            
+            deltaZ = Mathf.Abs(deltaZ);
+            if (deltaZ > ErrorMargin)
+            {
+                stackBounds.y -= deltaZ;
+                if (stackBounds.y <= 0)
+                {
+                    return false;
+                }
+
+                float middle = (prevBlockPosition.z + lastPosition.z) / 2;
+                lastBlock.localScale = new Vector3(stackBounds.x, 1, stackBounds.y);
+
+                Vector3 tempPosition = lastBlock.localPosition;
+                tempPosition.z = middle;
+                lastBlock.localPosition = lastPosition = tempPosition;
+                
+                float rubbleHalfScale = deltaZ / 2;
+                CreateRubble(
+                    new Vector3(
+                        lastPosition.x
+                        , lastPosition.y
+                        , isNegativeNum
+                            ? lastPosition.z + stackBounds.y / 2 + rubbleHalfScale
+                            : lastPosition.z - stackBounds.y / 2 - rubbleHalfScale),
+                    new Vector3(stackBounds.x, 1, deltaZ)
+                );
+            }
+            else
+            {
+                lastBlock.localPosition = prevBlockPosition + Vector3.up;
+            }
+        }
+
+        secondaryPosition = (isMovingX) ? lastBlock.localPosition.x : lastBlock.localPosition.z;
+
+        return true;
+    }
+
+    void CreateRubble(Vector3 pos, Vector3 scale)
+    {
+        GameObject go = Instantiate(lastBlock.gameObject);
+        go.transform.parent = this.transform;
+
+        go.transform.localPosition = pos;
+        go.transform.localScale = scale;
+        go.transform.localRotation = Quaternion.identity;
+
+        go.AddComponent<Rigidbody>();
+        go.name = "Rubble";
+    }
+    
 }
